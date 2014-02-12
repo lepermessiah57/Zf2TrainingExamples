ba8005320c40e3ab336eba0ce58c9c968ee39d17
Matt.Hill <lepermessiah57@gmail.com>
2014-02-12 13:34:25 -0800

    twigifing all the things

--- config/application.config.php
+++ config/application.config.php
@@ -8,7 +8,8 @@
 return array(
     'modules' => array(
         'Application',
-        'Calculator'
+        'Calculator',
+        'ZfcTwig'
         ),
     'module_listener_options' => array(
         'module_paths' => array(
